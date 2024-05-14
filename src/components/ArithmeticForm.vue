75ae7eb0ed842226265c305b27dd6f2ebe198f80
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-05-14 22:12:59 +0530

    Refactored ArithmeticForm component to use Tailwind CSS for styling

--- src/components/ArithmeticForm.vue
+++ src/components/ArithmeticForm.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <input v-model="number1" type="number" placeholder="Number 1" />
-    <input v-model="number2" type="number" placeholder="Number 2" />
-    <button @click="performOperation('add')">Add</button>
-    <button @click="performOperation('subtract')">Subtract</button>
-    <button @click="performOperation('multiply')">Multiply</button>
-    <button @click="performOperation('divide')">Divide</button>
-    <div v-if="result !== null">Result: {{ result }}</div>
+  <div class="flex flex-col items-center space-y-6">
+    <div class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+      <input class="border rounded px-4 py-2 w-full md:w-auto" v-model.number="number1" type="number" placeholder="Number 1" />
+      <input class="border rounded px-4 py-2 w-full md:w-auto" v-model.number="number2" type="number" placeholder="Number 2" />
+    </div>
+    <div class="flex flex-wrap justify-center space-x-2">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="performOperation('add')">Add</button>
+      <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" @click="performOperation('subtract')">Subtract</button>
+      <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" @click="performOperation('multiply')">Multiply</button>
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="performOperation('divide')">Divide</button>
+    </div>
+    <div v-if="result !== null" class="text-lg font-semibold">Result: {{ result }}</div>
   </div>
 </template>
 
