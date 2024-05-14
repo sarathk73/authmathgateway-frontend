face0e9f60dc91356223b4f908b3baa020787d17
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-05-14 21:27:58 +0530

    Updated Tailwind CSS content settings to include HTML and Vue files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
