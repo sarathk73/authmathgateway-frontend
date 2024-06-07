1e151dcda0f53dd2a768d5594626493617e830fc
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-06-07 18:24:00 +0530

    Refactor Logout Button

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,13 @@
           <div class="flex items-center justify-end gap-3">
             <a 
               v-if="isAuthenticated"
-              class="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              href="#_"
+              class="relative inline-block px-4 py-2 font-medium group"
               @click="logout"
             >
-              Logout
+              <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+              <span class="relative text-black group-hover:text-white">Logout</span>
             </a>
           </div>
         </div>
